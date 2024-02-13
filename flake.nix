b6569f301c0a6bb90fc354af4031292382120848
Theo Paris <theop@flowtr.dev>
2024-02-12 22:42:43 -0800

    build: add static zig package

--- flake.nix
+++ flake.nix
@@ -1,15 +1,9 @@
 {
-	description = "Zig compiler development.";
+	description = "The Zig compiler";
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
-
-		# Used for shell.nix
-		flake-compat = {
-			url = "github:edolstra/flake-compat";
-			flake = false;
-		};
 	};
 
 	outputs = {
@@ -20,26 +14,50 @@
 	} @ inputs:
 		flake-utils.lib.eachDefaultSystem (
 			system: let
-				pkgs = import nixpkgs {inherit system;};
+				pkgs = import nixpkgs { inherit system; };
 			in {
 				devShells.default = pkgs.mkShell {
-					nativeBuildInputs = with pkgs;
-						[
-							cmakeMinimal
-							libxml2
-							ninja
-							zlib
-							zstd
-							stdenv.cc.cc.lib
-						]
-						++ (with llvmPackages_17; [
-							clang
-							clang-unwrapped
-							lld
-							llvm
-						]);
+					nativeBuildInputs = with pkgs; [
+						cmakeMinimal
+						libxml2
+						ninja
+						zlib
+						zstd
+						stdenv.cc.cc.lib
+					] ++ (with llvmPackages_17; [
+						clang
+						clang-unwrapped
+						lld
+						llvm
+					]);
 
 					hardeningDisable = ["all"];
+				};
+
+				packages.default = pkgs.pkgsStatic.stdenv.mkDerivation {
+					name = "zig";
+					src = ./.;
+					buildInputs = with pkgs.pkgsStatic; [
+						libxml2
+						zlib
+						zstd
+						stdenv.cc.cc.lib
+					] ++ (with llvmPackages_17; [
+						clang
+						clang-unwrapped
+						lld
+						llvm
+					]);
+
+					nativeBuildInputs = with pkgs; [
+						ccache
+						ninja
+						cmakeMinimal
+					];
+
+					cmakeFlags = [
+						"-DZIG_USE_CCACHE=ON"
+					];
 				};
 			}
 		);
